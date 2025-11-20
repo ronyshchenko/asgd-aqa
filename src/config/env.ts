3c9eef59c75101277b10b8a12af946e27ac68f26
ronyshchenko <roma.onyshchenko@gmail.com>
2025-11-20 15:53:42 +0200

    two commit

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,8 +2,6 @@ import { ICredentials } from "data/types/credentials.types";
 
 export const ASGD_PORTAL_URL = "https://test-human-aid.ioc.gov.ua/admin";
 export const credentials: ICredentials = {
-  username: "ronyshchenko",
-  password: "270872",
-  // username: process.env.USER_NAME!,
-  // password: process.env.USER_PASSWORD!,
+  username: process.env.USER_NAME!,
+  password: process.env.USER_PASSWORD!,
 };
